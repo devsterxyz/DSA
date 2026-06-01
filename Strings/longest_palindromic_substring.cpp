798681fdc6d04b0e92af2fafa1833c36ecb246b0
Dev <sharmadevdeep9@gmail.com>
2026-06-01 10:28:32 +0530

    5. Longest Palindromic Substring

---
+++ Strings/longest_palindromic_substring.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <climits>
+#include <cstring>
+using namespace std;
+
+int t[1001][1001];
+
+bool solve(string &s, int i, int j){
+  if(i>=j){
+    return 1;
+  }
+  if(t[i][j] != -1){
+    return t[i][j];
+  }
+  if(s[i] == s[j]){
+    return t[i][j] = solve(s, i+1, j-1);
+  }
+  return t[i][j] = 0;
+}
+
+string longestPalindrome(string s) {
+  int n = s.length();
+  memset(t, -1, sizeof(t));
+  int maxLen = INT_MIN;
+  int sp = 0;
+
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      if(solve(s, i, j) == true){
+        if(j-i+1 > maxLen){
+          maxLen = j-i+1;
+          sp = i;
+        }
+      }
+    }
+  }
+
+  return s.substr(sp, maxLen);
+}
+
+
+int main() {
+
+  string s;
+  cout << "Enter the string: ";
+  cin >> s;
+
+  cout << longestPalindrome(s);
+
+ return 0;
+}
