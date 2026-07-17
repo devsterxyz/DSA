a97c1d893e32df31e14a91951d9e265f5809e72c
Dev <sharmadevdeep9@gmail.com>
2026-07-17 20:16:21 +0530

    tree intro

--- Stack/asteroid_collision.cpp
+++ Stack/asteroid_collision.cpp
@@ -3,36 +3,44 @@
 #include <stack>
 using namespace std;
 
-
-vector<int> asteroidCollision(vector<int>& asteroids) {
+vector<int> asteroidCollision(vector<int> &asteroids)
+{
   stack<int> st;
   vector<int> ans;
-  for(int i=0; i<asteroids.size(); i++){
+  for (int i = 0; i < asteroids.size(); i++)
+  {
     bool flag = false;
-    while(!st.empty() && asteroids[i] < 0){
+    while (!st.empty() && asteroids[i] < 0)
+    {
       int k = -asteroids[i];
       int prev = st.top();
-      if(prev < 0){
+      if (prev < 0)
+      {
         break;
       }
-      else if(prev > k){
+      else if (prev > k)
+      {
         flag = true;
         break;
       }
-      else if(prev < k){
+      else if (prev < k)
+      {
         st.pop();
       }
-      else{
+      else
+      {
         flag = true;
         st.pop();
         break;
       }
     }
-    if(!flag){
+    if (!flag)
+    {
       st.push(asteroids[i]);
     }
   }
-  while(!st.empty()){
+  while (!st.empty())
+  {
     int n = st.top();
     st.pop();
     ans.insert(ans.begin(), n);
@@ -40,8 +48,8 @@ vector<int> asteroidCollision(vector<int>& asteroids) {
   return ans;
 }
 
-
-int main() {
+int main()
+{
 
   int n;
   cout << "Enter the number of asteroids: ";
@@ -49,17 +57,19 @@ int main() {
 
   vector<int> asteroids(n);
   cout << "Enter the sizes of the asteroids (positive for right-moving, negative for left-moving):" << endl;
-  for(int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cin >> asteroids[i];
   }
 
   vector<int> result = asteroidCollision(asteroids);
-  
+
   cout << "Remaining asteroids after all collisions: ";
-  for(int size : result) {
+  for (int size : result)
+  {
     cout << size << " ";
   }
   cout << endl;
 
- return 0;
+  return 0;
 }
